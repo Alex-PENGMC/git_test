02ea1ef04ee5d4aa2dc96f4d3ca03dd18d58998c
Alex-PENGMC <alexpengmc@163.com>
2025-01-20 15:36:14 +0800

    1. 调整分支结构；2. 新增设置代码段的步骤； 3. 后续代码部分全部添加到这里

--- pengmc.h
+++ pengmc.h
@@ -5,16 +5,31 @@
 #include <time.h>
 #include <unistd.h>
 
-#define OK             0
-#define ERROR          -1
+#define OK             (0)
+#define ERROR          (-1)
+
+#define TRUE           (0)
+#define FALSE          (-1)
+
 
 #define _DEBUG_ 
 #ifdef _DEBUG_
-#define DBG_PRINTF(...) printf(__VA_ARGS__)
+    #define DBG_PRINTF(fmt, ...) \
+        printf("[DEBUG] [%s:%d] %s(): " fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #else
-#define DBG_PRINTF(...)
+    #define DBG_PRINTF(...)
 #endif
-#define SWAP(A, B) {int temp; temp = A; A = B; B = temp;}
+
+
+#define SWAP(A, B) do { int temp = A; A = B; B = temp; } while(0)
+#define MAX2(A, B) ((A) > (B) ? (A) : (B))
+#define MIN2(A, B) ((A) < (B) ? (A) : (B))
+#define MAX3(A, B, C) (MAX2((A), MAX2((B), (C))))
+#define MIN3(A, B, C) (MIN2((A), MIN2((B), (C))))
+#define MID3(A, B, C) (MAX2(MIN2((A), (B)), MIN2(MAX2((A), (B)), (C))))
+#define CLAMP(val, min, max) ((val) < (min) ? (min) : ((val) > (max) ? (max) : (val)))
+#define ABS(A) ((A) < 0 ? -(A) : (A))
+
 
 #define PRINT_ARRAY(array, length)\
 {\
